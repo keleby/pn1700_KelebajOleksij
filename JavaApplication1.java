cbd3122a751f2ed0f75d9fcf171f9d1025e65085
sobytek <aghlaboratorium1235@gmail.com>
2015-06-15 18:06:21 +0200

    to ja

---
+++ JavaApplication1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author student207
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    nazwisko{
+    public static void main(String[] args) {
+        System.out.print("Kelebaj Oleksij");
+        // TODO code application logic here
+    }
+    
+}
+
+   
+  }
